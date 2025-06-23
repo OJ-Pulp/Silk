5e6d3279c76c43857b766d4ac9fcb34b2e4101ba
bgarringer1 <brooke.garringer.1@gmail.com>
2025-06-23 15:35:29 -0400

    Tested Resolving Sprues

--- test_ChainWeaver.py
+++ test_ChainWeaver.py
@@ -215,7 +215,47 @@ def create_base_product_parts(base_products: List[Component], base_product_sprue
 
 # endregion
 
-base_products = create_base_products(1)
+# -------------------------------------------------------------------------------------------
+#                               RESOLVE_BASE_PRODUCT_SPRUES
+# -------------------------------------------------------------------------------------------
+# region RESOLVE_SPRUES
+def resolve_base_product_sprues(base_product_sprues: List[Component], base_product_sprue_edges: List[Requires], base_product_part_edges: List[Requires]):
+    
+    for base_product_sprue in base_product_sprues:
+
+        edge_count = 0
+        
+        for base_product_part_edge in base_product_part_edges:
+
+            assert isinstance(base_product_part_edge, Requires)
+
+            if base_product_part_edge.start_node == base_product_sprue:
+
+                edge_count += 1
+
+        if edge_count == 0:
+
+            base_product_sprues.remove(base_product_sprue)
+
+            logger.debug(f"Removed Sprue:")
+            logger.debug(base_product_sprue)
+            logger.debug(f"")
+
+            for base_product_sprue_edge in base_product_sprue_edges:
+            
+                if base_product_sprue_edge.end_node == base_product_sprue:
+
+                    base_product_sprue_edges.remove(base_product_sprue_edge)
+
+                    logger.debug(f"Removed Sprue Edge:")
+                    logger.debug(base_product_sprue)
+                    logger.debug(f"")
+
+    return base_product_sprues, base_product_sprue_edges
+
+# endregion
+
+base_products = create_base_products(500)
 base_product_sprues, base_product_sprue_edges = create_base_product_sprues(base_products)
 base_product_parts, base_product_part_edges = create_base_product_parts(base_products, base_product_sprues)
-
+base_product_sprues, base_product_sprue_edges = resolve_base_product_sprues(base_product_sprues, base_product_sprue_edges, base_product_part_edges)
