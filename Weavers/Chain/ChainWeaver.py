768961db500d331a8939b5245af359ef0505f2d8
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-07-01 14:27:47 -0400

    Relative to root path importing

--- Weavers/Chain/ChainWeaver.py
+++ Weavers/Chain/ChainWeaver.py
@@ -44,7 +44,7 @@ except ModuleNotFoundError as e:
 
 # Local
 try:
-    from .data_model import Component, Requires
+    from Weavers.Chain.data_model import Component, Requires
     #import input_utils
 except ModuleNotFoundError as e:
     logger.critical(f"{type(e).__name__}: Missing Required Local Module '{e.name}' -- Check that '{e.name}.py' is in the Same Directory as 'ChainWeaver.py' -- Exiting")
