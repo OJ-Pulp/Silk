cd3231982a31862aa4c88afcce7238eb5216aa53
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-07-23 16:35:41 -0400

    Work on Weaver.py for complete abstract CSV writing

--- Weavers/Weaver.py
+++ Weavers/Weaver.py
@@ -37,6 +37,7 @@ logging.getLogger("faker").setLevel(logging.INFO)
 FAKER_GEN = faker.Faker()
 logger.info("Module Variables Set")
 
+
 class Weaver(ABC):
     """
     Base class for all weavers.
@@ -44,21 +45,83 @@ class Weaver(ABC):
     Examples include ChainWeaver, DocWeaver, MapWeaver, etc.
     """
 
-
-    def __init__(
-        self,
-        nodes: List[Node],
-        edges: List[Edge]
-    ):
+    def __init__(self):
         """
         Initialize the Weaver with a name.
         :param name: The name of the weaver.
         """
 
-        self.nodes = nodes
-        self.edges = edges
+        self.node_output_path: Path
+        self.edge_output_path: Path
+        self.__set_output_path__()
+
+        self.nodes: List[Node]
+        self.edges: List[Edge]
 
         logger.info("Weaver Initialized")
+
+    def __set_output_path__(self):
+        """
+        Set the output path for the Weaver's output files.
+        """
+
+        self.node_output_path = Weaver.get_output_filename(
+            base_name="nodes", extension="csv"
+        )
+        self.edge_output_path = Weaver.get_output_filename(
+            base_name="edges", extension="csv"
+        )
+        logger.info(
+            f"Output path set to {self.node_output_path} and {self.edge_output_path}"
+        )
+
+    @staticmethod
+    def get_output_filename(
+        base_name: str, extension: str, output_dir: Path = Path("output")
+    ) -> Path:
+        output_dir.mkdir(parents=True, exist_ok=True)
+        index = 1
+        if not extension.startswith("."):
+            extension = "." + extension
+        while True:
+            filename = output_dir / f"test{index}_{base_name}{extension}"
+            if not filename.exists():
+                logger.warning(index)
+                return filename
+            index += 1
+
+    def write_to_csv(self, data: Node | Edge):
+        """
+        Write the generated node / edge to the output CSV file.
+
+        :param data: The Node or Edge object to write to the CSV file.
+        """
+
+        if isinstance(data, Node):
+            node_output_file = open(self.node_output_path, "a", newline="")
+            node_writer = csv.DictWriter(
+                node_output_file, fieldnames=data.__dict__.keys()
+            )
+
+            # Only write header if file is empty
+            if node_output_file.tell() == 0:
+                node_writer.writeheader()
+
+            node_writer.writerow(data.to_dict())
+            node_output_file.close()  # Don’t forget to close the file!
+
+        else:  # isinstance(data, Edge):
+            edge_output_file = open(self.edge_output_path, "a", newline="")
+            edge_writer = csv.DictWriter(
+                edge_output_file, fieldnames=data.__dict__.keys()
+            )
+
+            # Only write header if file is empty
+            if edge_output_file.tell() == 0:
+                edge_writer.writeheader()
+
+            edge_writer.writerow(data.to_dict())
+            edge_output_file.close()  # Don’t forget to close the file!
 
     @abstractmethod
     def weave(self):
@@ -80,38 +143,3 @@ class Weaver(ABC):
         Generate edges for the weaver.
         This method should be implemented by subclasses.
         """
-    
-    @staticmethod
-    def get_output_filename(base_name: str, extension: str, output_dir: Path = Path("output")) -> Path:
-        output_dir.mkdir(parents=True, exist_ok=True)
-        index = 1
-        if not extension.startswith("."):
-            extension = "." + extension
-        while True:
-            filename = output_dir / f"test{index}_{base_name}{extension}"
-            if not filename.exists():
-                logger.warning(index)
-                return filename
-            index += 1
-
-    def write_to_csv(self, data: List[dict]):
-        """
-        Write the generated nodes and edges to a CSV file.
-        This method should be implemented by subclasses.
-        :param path: The path to the CSV file.
-        """
-
-        path = Weaver.get_output_filename(base_name="components", extension="csv")
-        file = open(path, "a", newline="") # Open file once
-
-        writer = csv.DictWriter(file, fieldnames=data[0].keys())
-
-        # Only write header if file is empty
-        if file.tell() == 0:
-            writer.writeheader()
-
-        for dicts in data:
-            writer.writerow(dicts)
-
-        file.close() # Don’t forget to close the file!
-
