4c8a94233df58968a297f099519ea13c4ba76e3b
Corbin <63320116+Tw1ZZLER@users.noreply.github.com>
2025-07-24 14:41:12 -0400

    Started implementing data classes

--- Weavers/graph_model.py
+++ Weavers/graph_model.py
@@ -5,10 +5,12 @@ Graph model for representing nodes and edges in a database.
 from abc import ABC
 from typing import Optional
 import uuid
+from dataclasses import dataclass, field
 from pathlib import Path
 import pandas as pd
 
 
+@dataclass
 class Node(ABC):
     """
     Abstract base class for a node in the generated database.
@@ -23,32 +25,26 @@ class Node(ABC):
     This allows for flexibility in defining edges with different properties, say if you want an unweighted graph, you can exclude the weight attribute.
     """
 
-    id: str 
-    """Unique identifier for the node, generated if not provided."""
-
     name: str 
     """Name of the node to be displayed in GUI tools."""
 
-    weight: float = 1.0
+    id: str = field(default_factory=lambda:str(uuid.uuid4()), init=False)
+    """Unique identifier for the node, generated with UUID4 if not provided."""
+
+    weight: Optional[float] = field(default_factory=lambda:1.0, init=False)
     """Weight of the node, default is 1.0."""
 
-    def __init__(self, name: str, id: Optional[str] = None, weight: Optional[float] = 1.0):
-        """
-        Initialize a Node with a name.
-
-        :param name: The label or identifier of the node.
-        """
-        self.id = id if id is not None else str(uuid.uuid4())
-        self.name = name
-        # if weight is not None:
-            # self.weight = weight
-        self.metadata: dict
-
-    def __eq__(self, other):
-        return isinstance(other, Node) and self.id == other.id
-
-    def __hash__(self):
-        return hash(self.id)
+    # def __init__(self, name: str, id: Optional[str] = None, weight: Optional[float] = 1.0):
+    #     """
+    #     Initialize a Node with a name.
+    #
+    #     :param name: The label or identifier of the node.
+    #     """
+    #     self.id = id if id is not None else str(uuid.uuid4())
+    #     self.name = name
+    #     # if weight is not None:
+    #         # self.weight = weight
+    #     self.metadata: dict
 
     def to_dict(self) -> dict:
         """
@@ -72,14 +68,15 @@ class Node(ABC):
             else:
                 row[k] = v
 
-        # Add metadata
-        if hasattr(self, "metadata"):
-            for k, v in self.metadata.items():
-                row[k] = v
+        # # Add metadata
+        # if hasattr(self, "metadata"):
+        #     for k, v in self.metadata.items():
+        #         row[k] = v
 
         return row
 
 
+@dataclass
 class Edge(ABC):
     """
     Abstract class representing a relationship (edge) between two nodes.
@@ -98,44 +95,8 @@ class Edge(ABC):
     end_node: Node
     """The target node of the edge (must be a Node subclass)."""
 
-    # weight: Optional[float] = 1.0
-    # """The weight of the edge, default is 1.0."""
-
-    def __init__(self, start_node: Node, end_node: Node):
-        """
-        Create a directed edge from one node to another.
-
-        :param start_node: The source node (must be a Node subclass).
-        :param end_node: The target node (must be a Node subclass).
-        :param weight: The weight of the edge, default is 1.0.
-        """
-        self.start_node = start_node
-        self.end_node = end_node
-        # self.weight = weight if not None else 1.0
-
-    def __eq__(self, other):
-        """
-        Check equality with another Edge instance.
-
-        Args:
-            other (object): The object to compare with.
-
-        Returns:
-            bool: True if 'other' is an Edge and has the same start and end nodes, as well as weight, False otherwise.
-        """
-        return isinstance(other, Edge) and \
-        self.start_node.id == other.start_node.id and \
-        self.end_node.id == other.end_node.id and \
-        self.weight == other.weight
-
-    def __hash__(self):
-        """
-        Compute the hash value for the Edge instance.
-
-        Returns:
-            int: The hash of the start_node ID, end_node ID, and weight.
-        """
-        return hash(self.start_node.id + self.end_node.id + str(self.weight))
+    weight: Optional[float] = field(default_factory=lambda:1.0, init=False)
+    """The weight of the edge, default is 1.0."""
 
     def to_dict(self) -> dict:
         """
